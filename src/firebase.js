f2d9ef4bb9ec6b3a302a716664d230080193f38e
nenevarder <dqphi@sdc.udn.vn>
2021-03-09 17:37:18 +0700

    create

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDrflUh9HoqYvhjsQk16lHMSwBWISWFUi8",
+    authDomain: "slack-clone-fa38d.firebaseapp.com",
+    projectId: "slack-clone-fa38d",
+    storageBucket: "slack-clone-fa38d.appspot.com",
+    messagingSenderId: "454346781865",
+    appId: "1:454346781865:web:b8192862100c936e0ba5b8"
+};
